3b54dd9b52938424e8166a8dfb40779a54463989
GibberishDev <gibberishdevelopment@gmail.com>
2026-03-20 23:59:44 +0300

    Settings menu implementation

--- awesome_chat_combos/scripts/index.js
+++ awesome_chat_combos/scripts/index.js
@@ -6,17 +6,25 @@ setModuleList([
 ])
 
 new SettingArray("channels", [], "Channels", "List of channels to trach chat messages from")
-new SettingInt("numberregular", 2, -1, null, 1,"Required repetitions: Regular", "Minimum number of repeats in chat to display combo. -1 means disabled")
-new SettingInt("numbermega", 10, -1, null, 1,"Required repetitions: Mega", "Minimum number of repeats in chat to display mega combo animation. -1 means disabled")
-new SettingInt("numbersuper", 25, -1, null, 1,"Required repetitions: Super", "Minimum number of repeats in chat to display super combo animation. -1 means disabled")
-new SettingInt("durationregular", 10000, -1, null, 100,"Duration: Regular", "Time in milliseconds until combo expires. Values below 2500 practically mean combo expires before twitch api sends information")
-new SettingInt("durationmega", 15000, -1, null, 100,"Duration: Mega", "Time in milliseconds until mega combo expires. Values below 2500 practically mean combo expires before twitch api sends information")
-new SettingInt("durationsuper", 20000, -1, null, 100,"Duration: Super", "Time in milliseconds until super combo expires. Values below 2500 practically mean combo expires before twitch api sends information")
-new SettingBool("showtimer", true, "Display timer", "Decides whether to show or hide combo expiration timer")
 new SettingColor("textcolor", "#ffff00ff", "Text color", "Text color of regular combo")
-new SettingInt("lettersnumber", 2, -1, null, 1, "Required repetitions: Regular", "Minimum number of repeats in chat to display combo. -1 means disabled")
+new SettingBool("showtimer", true, "Display timer", "Decides whether to show or hide combo expiration timer")
+new SettingBool("showcounter", true, "Display counter", "Decides whether to show or hide combo counter")
+new SettingBool("displayemote", true, "Display counter", "Decides whether to show or hide combo counter")
+new SettingNumber("lettersnumber", 20, -1, null, 1, "Visible letters", "Maximum amount of visible letters when displaying a word combo. -1 to display all")
+new SettingNumber("numberregular", 2, -1, null, 1,"Required repetitions: Regular", "Minimum number of repeats in chat to display combo. -1 means disabled")
+new SettingNumber("numbermega", 10, -1, null, 1,"Required repetitions: Mega", "Minimum number of repeats in chat to display mega combo animation. -1 means disabled")
+new SettingNumber("numbersuper", 25, -1, null, 1,"Required repetitions: Super", "Minimum number of repeats in chat to display super combo animation. -1 means disabled")
+new SettingNumber("durationregular", 10000, 0, null, 100,"Duration: Regular", "Time in milliseconds until combo expires. Values below 2500 practically mean combo expires before twitch api sends information")
+new SettingNumber("durationmega", 15000, 0, null, 100,"Duration: Mega", "Time in milliseconds until mega combo expires. Values below 2500 practically mean combo expires before twitch api sends information")
+new SettingNumber("durationsuper", 20000, 0, null, 100,"Duration: Super", "Time in milliseconds until super combo expires. Values below 2500 practically mean combo expires before twitch api sends information")
 
 initSettings()
 // #endregion
 
 if (registeredSettings.get("channels").value = []) console.info("No channel set. Abborting startup")
+
+function triggerReflow(element) {
+    element.style.animation = 'none';
+    void element.offsetHeight;
+    element.style.animation = null;
+}
--- awesome_chat_combos/scripts/settings.js
+++ awesome_chat_combos/scripts/settings.js
@@ -1,5 +1,5 @@
 let registeredSettings = new Map()
-
+var settingsOpen = false
 var params = new URLSearchParams(window.location.search)
 
 function initSettings() {
@@ -16,6 +16,7 @@ function retrieveSettings() {
             }
         }
     }
+    saveSettings()
 }
 
 function saveSettings() {
@@ -30,10 +31,9 @@ function saveSettings() {
 const SETTING_TYPE = Object.freeze({
     STRING : 0,
     ARRAY : 1,
-    INT : 2,
-    FLOAT : 3,
-    BOOL : 4,
-    COLOR : 5,
+    NUMBER : 2,
+    BOOL : 3,
+    COLOR : 4,
 })
 
 class Setting {
@@ -47,7 +47,11 @@ class Setting {
         if (registeredSettings.get(id)) console.warn("Setting with id '" + id + "' already exists. Overwriting...")
         registeredSettings.set(id, this)
     }
-    set(value) { this.value = value }
+    get() {return this.value}
+    set(value) {
+        this.value = value
+        console.log(this.id, this.value)
+    }
 }
 
 class SettingArray extends Setting {
@@ -70,24 +74,320 @@ class SettingColor extends Setting {
         super (id, SETTING_TYPE.COLOR, defaultValue, name, description)
     }
 }
-class SettingInt extends Setting {
+class SettingNumber extends Setting {
     constructor (id, defaultValue, minValue=null, maxValue=null, step=1, name="Unnamed setting", description="No description provided.") {
-        super (id, SETTING_TYPE.ARRAY, defaultValue, name, description)
+        super (id, SETTING_TYPE.NUMBER, defaultValue, name, description)
+        this.minValue = minValue
+        this.maxValue = maxValue
+        this.step = step
     }
     set(value) {
         if (isNaN(parseInt(value))) {
-            console.error("ERROR: Cannot set value " + value + " to setting " + id + ". Not an Integer")
             return false
         }
         if (this.minValue) { if (value < this.minValue) value = this.minValue }
         if (this.maxValue) { if (value > this.maxValue) value = this.maxValue }
         if (this.step) if (value % this.step != 0) { value = Math.round(value / this.step) * this.step }
         this.value = value
+        console.log("value set for " + this.id + ": " + this.value)
+        return value
     }
 }
 // #region html menu gen
+const templateString = `
+<div class="setting-title"></div>
+<div class="setting-description"></div>
+<div class="setting-data">
+    <input class="input" value="" placeholder="">
+</div>
+`
+const templateToggle = `
+<div class="setting-title"></div>
+<div class="setting-description"></div>
+<div class="setting-data">
+    <input class="input" type="checkbox">
+    <div class="checkbox-vis"></div>
+</div>
+`
+const templateColor = `
+<div class="setting-title"></div>
+<div class="setting-description"></div>
+<div class="setting-data">
+    <div class="color-preview"></div>
+    <div class="slider hue" data-value="0">
+        <div class="grabber"></div>
+    </div>
+    <div class="slider sat" data-value="0">
+        <div class="grabber"></div>
+    </div>
+    <div class="slider val" data-value="0">
+        <div class="grabber"></div>
+    </div>
+</div>
+`
 function genSettingsMenu() {
-
+    let elementsList = []
+    document.querySelector("#open-settings").style.display = "none"
+    document.querySelector("#settings-wrapper").classList.remove("hidden")
+    document.querySelector("#settings-container").innerHTML = ""
+    for (let id of registeredSettings.keys()) {
+        let setting = registeredSettings.get(id)
+        switch (setting.type) {
+            case SETTING_TYPE.STRING : {
+                let el = document.createElement('div')
+                document.querySelector("#settings-container").appendChild(el)
+                el.classList.add("settings-item","string")
+                el.innerHTML = templateString
+                el.querySelector(".setting-title").textContent = setting.name
+                el.querySelector(".setting-description").textContent = setting.description
+                el.querySelector(".seting-data input").value = setting.get()
+                el.querySelector(".seting-data input").placeholder = "Enter string value here"
+                hookStringInput(el.querySelector(".seting-data input"), setting.id)
+                break
+            }
+            case SETTING_TYPE.ARRAY : {
+                let el = document.createElement('div')
+                document.querySelector("#settings-container").appendChild(el)
+                el.classList.add("settings-item","string")
+                el.innerHTML = templateString
+                el.querySelector(".setting-title").textContent = setting.name
+                el.querySelector(".setting-description").textContent = setting.description
+                el.querySelector(".input").value = setting.get().toString()
+                el.querySelector(".input").placeholder = "Enter comma separated values here"
+                hookArrayInput(el.querySelector(".input"), setting.id)
+                break
+            }
+            case SETTING_TYPE.NUMBER : {
+                let el = document.createElement('div')
+                document.querySelector("#settings-container").appendChild(el)
+                el.classList.add("settings-item","string")
+                el.innerHTML = templateString
+                el.querySelector(".setting-title").textContent = setting.name
+                el.querySelector(".setting-description").textContent = setting.description
+                el.querySelector(".input").type = "number"
+                if (setting.step) {
+                    el.querySelector(".input").step = setting.step
+                }
+                if (setting.minValue) {
+                    el.querySelector(".input").min = setting.minValue
+                }
+                if (setting.maxValue) {
+                    el.querySelector(".input").max = setting.maxValue
+                }
+                el.querySelector(".input").value = setting.get()
+                hookNumberInput(el.querySelector(".input"), setting.id)
+                break
+            }
+            case SETTING_TYPE.BOOL : {
+                let el = document.createElement('div')
+                document.querySelector("#settings-container").appendChild(el)
+                el.classList.add("settings-item","checkbox")
+                el.innerHTML = templateToggle
+                el.querySelector(".setting-title").textContent = setting.name
+                el.querySelector(".setting-description").textContent = setting.description
+                el.querySelector(".input").checked = setting.get()
+                hookBoolInput(el.querySelector(".input"), setting.id)
+                break
+            }
+            case SETTING_TYPE.COLOR : {
+                let el = document.createElement('div')
+                document.querySelector("#settings-container").appendChild(el)
+                el.classList.add("settings-item","color")
+                el.innerHTML = templateColor
+                el.querySelector(".setting-title").textContent = setting.name
+                el.querySelector(".setting-description").textContent = setting.description
+                hookColorInput(el.querySelector(".setting-data"), setting.id)
+                break
+            }
+        }
+    }
 }
-const templateArray = ``
+
+function hookStringInput(el, id) {
+    el.onblur = () => {
+        registeredSettings.get(id).set(el.value)
+    }
+}
+function hookArrayInput(el, id) {
+    el.onblur = () => {
+        let values = []
+        el.value.split(",").forEach((item)=>values.push(item.trim()))
+        registeredSettings.get(id).set(values)
+    }
+}
+function hookNumberInput(el, id) {
+    el.onfocus = ()=>{
+        el.dataset.lastValue = registeredSettings.get(id).get()
+    }
+    el.onblur = () => {
+        if (registeredSettings.get(id).set(el.value) != false) {
+            el.value = registeredSettings.get(id).get()
+        } else {
+            el.value = el.dataset.lastValue
+        }
+    }
+}
+function hookBoolInput(el, id) {
+    el.onchange = ()=>{
+        if (el.checked) {
+            registeredSettings.get(id).set(true)
+        } else {
+            registeredSettings.get(id).set(false)
+        }
+    }
+}
+function hookColorInput(el, id) {
+    let hue = el.querySelector(".hue")
+    let sat = el.querySelector(".sat")
+    let val = el.querySelector(".val")
+    let color = new HSV().fromHex(registeredSettings.get(id).get())
+    hue.dataset.value = color.h/360
+    sat.dataset.value = color.s/100
+    val.dataset.value = color.v/100
+    hue.querySelector(".grabber").style.left = (color.h/360*(hue.getBoundingClientRect().width - 10)) + "px"
+    sat.querySelector(".grabber").style.left = (color.s/100*(hue.getBoundingClientRect().width - 10)) + "px"
+    val.querySelector(".grabber").style.left = (color.v/100*(hue.getBoundingClientRect().width - 10)) + "px"
+    el.querySelector(".color-preview").style.setProperty("background",registeredSettings.get(id).get())
+    hue.addEventListener("mousedown",(event)=>{
+        handleColorDrag(event, hue, id)
+        let inputEl = document.createElement("div")
+        inputEl.classList.add("cover")
+        document.body.appendChild(inputEl)
+        inputEl.addEventListener("mousemove",(ev)=>handleColorDrag(ev, hue, id),{capture:true})
+        document.addEventListener("mouseup",()=>{inputEl.remove()},{once:true})
+    })
+    sat.addEventListener("mousedown",(event)=>{
+        handleColorDrag(event, sat, id)
+        let inputEl = document.createElement("div")
+        inputEl.classList.add("cover")
+        document.body.appendChild(inputEl)
+        inputEl.addEventListener("mousemove",(ev)=>handleColorDrag(ev, sat, id),{capture:true})
+        document.addEventListener("mouseup",()=>{inputEl.remove()},{once:true})
+    })
+    val.addEventListener("mousedown",(event)=>{
+        handleColorDrag(event, val, id)
+        let inputEl = document.createElement("div")
+        inputEl.classList.add("cover")
+        document.body.appendChild(inputEl)
+        inputEl.addEventListener("mousemove",(ev)=>handleColorDrag(ev, val, id),{capture:true})
+        document.addEventListener("mouseup",()=>{inputEl.remove()},{once:true})
+    })
+}
+function handleColorDrag(event,el,id) {
+    let x = event.clientX - 5
+    let rect = el.getBoundingClientRect()
+    x -= rect.x
+    x = Math.min(rect.width - 10, Math.max(0, x))
+    el.querySelector(".grabber").style.left = x + "px"
+    let val = x/(rect.width - 10)
+    el.dataset.value = val
+    let h = el.parentNode.querySelector(".hue").dataset.value * 360
+    let s = el.parentNode.querySelector(".sat").dataset.value * 100
+    let v = el.parentNode.querySelector(".val").dataset.value * 100
+    let color = new HSV(h,s,v)
+    el.parentNode.querySelector(".sat").style.setProperty("--hue",new HSV(h,100,v).toHex())
+    el.parentNode.querySelector(".sat").style.setProperty("--val",new HSV(h,0,v).toHex())
+    el.parentNode.querySelector(".val").style.setProperty("--sat",new HSV(h,s,100).toHex())
+    el.parentNode.querySelector(".color-preview").style.setProperty("background",color.toHex())
+    registeredSettings.get(id).set(color.toHex())
+}
+class HSV {
+    constructor(h,s,v) {
+        this.h = h
+        this.s = s
+        this.v = v
+    }
+    toHex = () => {
+        var sat = this.s / 100;
+        var val = this.v / 100;
+
+        const c = val * sat;
+        const x = c * (1 - Math.abs((this.h / 60) % 2 - 1));
+        const m = val - c;
+
+        let r = 0, g = 0, b = 0;
+
+        if (this.h >= 0 && this.h < 60) {
+            r = c; g = x; b = 0;
+        } else if (this.h < 120) {
+            r = x; g = c; b = 0;
+        } else if (this.h < 180) {
+            r = 0; g = c; b = x;
+        } else if (this.h < 240) {
+            r = 0; g = x; b = c;
+        } else if (this.h < 300) {
+            r = x; g = 0; b = c;
+        } else {
+            r = c; g = 0; b = x;
+        }
+
+        const toHex = n =>
+            Math.round((n + m) * 255)
+            .toString(16)
+            .padStart(2, "0");
+
+        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    }
+
+    fromHex = (hex) => {
+        hex = hex.replace(/^#/, "");
+
+        const r = parseInt(hex.slice(0, 2), 16) / 255;
+        const g = parseInt(hex.slice(2, 4), 16) / 255;
+        const b = parseInt(hex.slice(4, 6), 16) / 255;
+
+        const max = Math.max(r, g, b);
+        const min = Math.min(r, g, b);
+        const delta = max - min;
+
+        let hue = 0;
+        let sat = 0;
+        let val = max;
+
+        if (delta !== 0) {
+            sat = delta / max;
+
+            switch (max) {
+            case r:
+                hue = ((g - b) / delta) % 6;
+                break;
+            case g:
+                hue = (b - r) / delta + 2;
+                break;
+            case b:
+                hue = (r - g) / delta + 4;
+                break;
+            }
+
+            hue *= 60;
+            if (hue < 0) hue += 360;
+        }
+        this.h = hue,
+        this.s = sat * 100,
+        this.v = val * 100
+        return this
+    }
+}
+function inputSaveSettings() {
+    saveSettings()
+    document.querySelector("#settings-container").innerHTML = ""
+    document.querySelector("#settings-wrapper").classList.add("hidden")
+    settingsOpen = false
+    document.querySelector("#open-settings").style.display = ""
+}
+function inputCancelSettigns() {
+    retrieveSettings()
+    document.querySelector("#settings-container").innerHTML = ""
+    document.querySelector("#settings-wrapper").classList.add("hidden")
+    settingsOpen = false
+    document.querySelector("#open-settings").style.display = ""
+}
+function inputResetSettings() {
+    for (let id of registeredSettings.keys()) {
+        registeredSettings.get(id).set(registeredSettings.get(id).defaultValue)
+    }
+    saveSettings()
+    genSettingsMenu()
+}
+document.addEventListener("mousemove",()=>{triggerReflow(document.querySelector("#open-settings"))})
 // #endregion
